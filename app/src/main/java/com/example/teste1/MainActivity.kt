d2f1e845d73c3fc17f9082ce747b750ee4ba033a
RodrigoMiranda <r.miranda@ipvc.pt git config --global user.email r.miranda@ipvc.pt>
2022-10-03 12:00:41 +0100

    add update

--- app/src/main/java/com/example/teste1/MainActivity.kt
+++ app/src/main/java/com/example/teste1/MainActivity.kt
@@ -12,6 +12,7 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         Toast.makeText(this, R.string.welcome, Toast.LENGTH_SHORT).show()
     }
+    // sassasa
 /*
     override fun onStart() {
         super.onStart()
